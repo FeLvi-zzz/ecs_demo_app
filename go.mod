9e305d7cf77c46e23a88eeab8912ef542e58a24d
FeLvi_zzz <34182539+FeLvi-zzz@users.noreply.github.com>
2019-09-12 15:51:04 +0900

    add zipcode func

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module github.com/grandcolline/ecs_demo_app
 
 require github.com/aws/aws-sdk-go v1.15.68
+require github.com/aws/aws-xray-sdk-go v1.0.0-rc.14
--- main.go
+++ main.go
@@ -13,7 +13,17 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
+
+	"github.com/aws/aws-xray-sdk-go/xray"
+	_ "github.com/aws/aws-xray-sdk-go/plugins/ecs"
 )
+
+func init() {
+  xray.Configure(xray.Config{
+    DaemonAddr:       "127.0.0.1:2000",
+		ServiceVersion:   "1.2.3",
+	})
+}
 
 func main() {
 	flag.Parse()
@@ -21,12 +31,20 @@ func main() {
 		fmt.Println("ok")
 	} else {
 		fmt.Println("=== Application API Starting!!")
-		http.HandleFunc("/hc", healthHandler)
-		http.HandleFunc("/info", infoHandler)
-		http.HandleFunc("/fibo", fiboHandler)
-		http.HandleFunc("/down", downHandler)
+		http.Handle("/",        xray.Handler(xray.NewFixedSegmentNamer("index"),   http.HandlerFunc(notFoundHandler)))
+		http.Handle("/hc",      xray.Handler(xray.NewFixedSegmentNamer("health"),  http.HandlerFunc(healthHandler)))
+		http.Handle("/info",    xray.Handler(xray.NewFixedSegmentNamer("info"),    http.HandlerFunc(infoHandler)))
+		http.Handle("/fibo",    xray.Handler(xray.NewFixedSegmentNamer("fibo"),    http.HandlerFunc(fiboHandler)))
+		http.Handle("/zipcode", xray.Handler(xray.NewFixedSegmentNamer("zipcode"), http.HandlerFunc(zipcodeHandler)))
+		http.Handle("/down",    xray.Handler(xray.NewFixedSegmentNamer("down"),    http.HandlerFunc(downHandler)))
 		http.ListenAndServe(":8080", nil)
 	}
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("--- notFoundHandler")
+	w.WriteHeader(http.NotFound)
+	fmt.Fprint(w, "404 Not Found")
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
@@ -75,6 +93,40 @@ func fiboHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprint(w, strconv.Itoa(n)+"番目のフィボナッチ数は、"+strconv.Itoa(fibo(n)))
+}
+
+func zipcodeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("--- zipcodeHandler")
+
+	ctx := r.Context()
+
+	zipcode, err := strconv.Atoi(r.URL.Query().Get("zipcode"))
+	if err != nil {
+		fmt.Fprint(w, "ERROR")
+		return
+	}
+
+	myClient := xray.Client(http.DefaultClient)
+
+	req, err := http.NewRequest(http.MethodGet, "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + strconv.Itoa(zipcode), nil)
+	if err != nil {
+		fmt.Errorf("[BUG] failed to build request: %s", err)
+		return
+	}
+	resp, err := myClient.Do(req.WithContext(ctx))
+
+	fmt.Fprint(w, "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + strconv.Itoa(zipcode))
+	if err != nil {
+		fmt.Fprint(w, "ERROR")
+		return
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+  if err == nil {
+    fmt.Fprint(w, string(b))
+  }
+
 }
 
 func downHandler(w http.ResponseWriter, r *http.Request) {
